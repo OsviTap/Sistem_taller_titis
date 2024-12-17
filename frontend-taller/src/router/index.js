bc217b36985676938be31cf956261737fcba5719
osvitap <johan.orellana.tapia@gmail.com>
2024-12-17 12:32:19 -0400

    modificacion en axios y creacion de index.js

--- frontend-taller/src/router/index.js
+++ frontend-taller/src/router/index.js
@@ -1,23 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+import Dashboard from '@/views/Dashboard.vue';
+import Login from '@/views/Login.vue';
+import Clientes from '@/views/Clientes.vue';
+import Vehiculos from '@/views/Vehiculos.vue';
+import Inventario from '@/views/Inventario.vue';
+import Reportes from '@/views/Reportes.vue';
 
-export default router
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/dashboard', component: Dashboard },
+  { path: '/clientes', component: Clientes },
+  { path: '/vehiculos', component: Vehiculos },
+  { path: '/inventario', component: Inventario },
+  { path: '/reportes', component: Reportes },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+export default router;

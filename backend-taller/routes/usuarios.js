888155e0598bff7050f6b3cb0d033725b4facf8e
osvitap <johan.orellana.tapia@gmail.com>
2025-01-03 17:44:54 -0400

    Refactor package.json and add postinstall script

--- backend-taller/routes/usuarios.js
+++ backend-taller/routes/usuarios.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Usuario = require('../models/Usuario');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Middleware de autenticación
 const authenticateToken = (req, res, next) => {
